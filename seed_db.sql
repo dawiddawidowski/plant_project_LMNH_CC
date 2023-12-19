dcdeb820b20c45df8bdc6247a18275d25d2c04ed
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2023-12-19 11:16:01 +0000

    Created all tables

--- seed_db.sql
+++ seed_db.sql
@@ -2,18 +2,13 @@
 -- USE my_database;
 -- GO
 
--- Ensure starting with an empty schema
-DROP TABLE IF EXISTS s_gamma.origin;
-GO
-DROP TABLE IF EXISTS s_gamma.botanist;
-GO
-DROP TABLE IF EXISTS s_gamma.license;
-GO
-DROP TABLE IF EXISTS s_gamma.image;
-GO
-DROP TABLE IF EXISTS s_gamma.plant;
-GO
+-- Ensure starting with an empty schema (dependent tables first)
 DROP TABLE IF EXISTS s_gamma.reading;
+DROP TABLE IF EXISTS s_gamma.plant;
+DROP TABLE IF EXISTS s_gamma.image;
+DROP TABLE IF EXISTS s_gamma.license;
+DROP TABLE IF EXISTS s_gamma.botanist;
+DROP TABLE IF EXISTS s_gamma.origin;
 GO
 
 -- Create tables
@@ -41,7 +36,7 @@ CREATE TABLE IF NOT EXISTS license (
     license_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     url TEXT NOT NULL,
-    number smallint NOT NULL,
+    number SMALLINT NOT NULL,
 );
 
 
@@ -63,12 +58,20 @@ CREATE TABLE IF NOT EXISTS plant (
     scientific_name VARCHAR(100) NOT NULL,
     origin_id INT NOT NULL,
     image_id INT NOT NULL,
-    CONSTRAINT fk_origin_id FOREIGN KEY (origin_id) REFERENCES s_gamma.origin (origin_id)
+    CONSTRAINT fk_origin_id FOREIGN KEY (origin_id) REFERENCES s_gamma.origin (origin_id),
     CONSTRAINT fk_image_id FOREIGN KEY (image_id) REFERENCES s_gamma.image (image_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS reading (
     reading_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-    word TEXT NOT NULL
+    plant_id INT NOT NULL,
+    botanist_id INT NOT NULL,
+    soil_moisture DECIMAL(4,1) NOT NULL,
+    temperature DECIMAL(3,1) NOT NULL,
+    last_watered TIMESTAMPTZ NOT NULL,
+    recording_taken TIMESTAMPTZ NOT NULL,
+    CONSTRAINT fk_plant_id FOREIGN KEY (plant_id) REFERENCES s_gamma.plant (plant_id),
+    CONSTRAINT fk_botanist_id FOREIGN KEY (botanist_id) REFERENCES s_gamma.botanist(botanist_id)
+
 );
