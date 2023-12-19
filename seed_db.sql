69cb7dd84979f312338796cea243192f02abc203
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2023-12-19 10:14:07 +0000

    Dropping tables if exist

--- seed_db.sql
+++ seed_db.sql
@@ -5,7 +5,16 @@
 -- Ensure starting with an empty schema
 DROP TABLE IF EXISTS s_gamma.origin;
 GO
-
+DROP TABLE IF EXISTS s_gamma.botanist;
+GO
+DROP TABLE IF EXISTS s_gamma.license;
+GO
+DROP TABLE IF EXISTS s_gamma.image;
+GO
+DROP TABLE IF EXISTS s_gamma.plant;
+GO
+DROP TABLE IF EXISTS s_gamma.reading;
+GO
 
 -- Create tables
 CREATE TABLE IF NOT EXISTS origin (
