7bf63bd9423838abf198e86473beadfb92b5d840
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2023-12-19 11:04:06 +0000

    Created tables except reading

--- seed_db.sql
+++ seed_db.sql
@@ -47,19 +47,24 @@ CREATE TABLE IF NOT EXISTS license (
 
 CREATE TABLE IF NOT EXISTS image (
     image_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-    word TEXT NOT NULL
-);
-
-
-CREATE TABLE IF NOT EXISTS plant (
-    plant_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
     medium_url TEXT NOT NULL,
     regular_url TEXT NOT NULL,
     original_url TEXT NOT NULL,
     small_url TEXT NOT NULL,
     thumbnail_url TEXT NOT NULL,
+    license_id INT NOT NULL,
     CONSTRAINT fk_license_id FOREIGN KEY (license_id) REFERENCES s_gamma.license (license_id)
-    license_id INT NOT NULL REFERENCES s_gamma.license(license_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS plant (
+    plant_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+    plant_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    origin_id INT NOT NULL,
+    image_id INT NOT NULL,
+    CONSTRAINT fk_origin_id FOREIGN KEY (origin_id) REFERENCES s_gamma.origin (origin_id)
+    CONSTRAINT fk_image_id FOREIGN KEY (image_id) REFERENCES s_gamma.image (image_id)
 );
 
 
