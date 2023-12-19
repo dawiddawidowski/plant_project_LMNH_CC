8e10b625ffb24bf679953f39154fe294e1ce9888
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2023-12-19 10:07:14 +0000

    Started seed_db script

---
+++ seed_db.sql
@@ -0,0 +1,38 @@
+-- -- Use database
+-- USE my_database;
+-- GO
+
+-- Ensure starting with an empty schema
+DROP TABLE IF EXISTS s_gamma.origin;
+GO
+
+
+-- Create tables
+CREATE TABLE IF NOT EXISTS origin (
+
+);
+
+
+CREATE TABLE IF NOT EXISTS botanist (
+
+);
+
+
+CREATE TABLE IF NOT EXISTS license (
+
+);
+
+
+CREATE TABLE IF NOT EXISTS image (
+
+);
+
+
+CREATE TABLE IF NOT EXISTS plant (
+
+);
+
+
+CREATE TABLE IF NOT EXISTS reading (
+
+);
