1d4dd6c273f1fc45304779f8c0e934db894ff4f6
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2023-12-21 15:40:43 +0000

    Changing testing suite to pytest from unittest class style tests

--- test_transform_readings.py
+++ test_transform_readings.py
@@ -156,118 +156,169 @@ def fake_df_high_soil_temp():
     return pd.DataFrame(fake_plants)
 
 
-class TestCleanReadingData():
-    """Tests involving clean_reading_data()."""
+@pytest.fixture
+def fake_df_error():
+    """Fixture for testing error reading edge cases."""
 
-    def test_last_watered_datetime(self, fake_df):
-        """The values in the last_watered column should be converted to a datetime64[ns] type."""
+    fake_plants = [{
+        "botanist": None,
+        "last_watered": None,
+        "plant_name": None,
+        "plant_id": 8,
+        "error": 'plant not found',
+        "recording_taken": None,
+        "soil_moisture": None,
+        "temperature": None
+    },
+        {
+        "botanist": {
+            "email": "fake_email_2",
+            "name": "fake_name_2",
+            "phone": "fake_phone_2"
+        },
+        "last_watered": "Wed, 20 Dec 2023 14:10:54 GMT",
+        "plant_name": "fake_flower_2",
+        "plant_id": 2,
+        "error": None,
+        "recording_taken": "2023-12-21 10:20:34",
+        "soil_moisture": 30.91501666242039,
+        "temperature": 9.150493582669517
+    }]
 
-        result_df = clean_reading_data(fake_df)
-        assert result_df['last_watered'].dtype == 'datetime64[ns]'
+    return pd.DataFrame(fake_plants)
 
-    def test_soil_moisture_rounded(self, fake_df):
-        """The values in the soil_moisture column should be rounded to 2 d.p."""
 
-        result_df = clean_reading_data(fake_df)
-        assert result_df['soil_moisture'][0] == 33.96
-        assert result_df['soil_moisture'][1] == 30.92
+def test_last_watered_datetime(fake_df):
+    """The values in the last_watered column should be converted to a datetime64[ns] type."""
 
-    def test_temperature_rounded(self, fake_df):
-        """The values in the temperature column should be rounded to 2 d.p."""
+    result_df = clean_reading_data(fake_df)
+    assert result_df['last_watered'].dtype == 'datetime64[ns]'
 
-        result_df = clean_reading_data(fake_df)
-        assert result_df['temperature'][0] == 9.11
-        assert result_df['temperature'][1] == 9.15
 
-    def test_extracted_botanist_email_column(self, fake_df):
-        """The botanist column should be flattened to include a botanist_email column."""
+def test_soil_moisture_rounded(fake_df):
+    """The values in the soil_moisture column should be rounded to 2 d.p."""
 
-        result_df = clean_reading_data(fake_df)
-        assert result_df['botanist_email'][0] == "fake_email_1"
-        assert result_df['botanist_email'][1] == "fake_email_2"
+    result_df = clean_reading_data(fake_df)
+    assert result_df['soil_moisture'][0] == 33.96
+    assert result_df['soil_moisture'][1] == 30.92
 
-    def test_extracted_botanist_name_column(self, fake_df):
-        """The botanist column should be flattened to include a botanist_name column."""
 
-        result_df = clean_reading_data(fake_df)
-        assert result_df['botanist_name'][0] == "fake_name_1"
-        assert result_df['botanist_name'][1] == "fake_name_2"
+def test_temperature_rounded(fake_df):
+    """The values in the temperature column should be rounded to 2 d.p."""
 
-    def test_extracted_botanist_mobile_column(self, fake_df):
-        """The botanist column should be flattened to include a botanist_mobile column."""
+    result_df = clean_reading_data(fake_df)
+    assert result_df['temperature'][0] == 9.11
+    assert result_df['temperature'][1] == 9.15
 
-        result_df = clean_reading_data(fake_df)
-        assert result_df['botanist_mobile'][0] == "fake_phone_1"
-        assert result_df['botanist_mobile'][1] == "fake_phone_2"
 
-    def test_column_order(self, fake_df):
-        """
-        The resulting data frame columns should be in a specific order
-        for compatibility with load script.
-        """
+def test_extracted_botanist_email_column(fake_df):
+    """The botanist column should be flattened to include a botanist_email column."""
 
-        result_df = clean_reading_data(fake_df)
-        assert result_df.columns[0] == 'plant_id'
-        assert result_df.columns[1] == 'plant_name'
-        assert result_df.columns[2] == 'soil_moisture'
-        assert result_df.columns[3] == 'temperature'
-        assert result_df.columns[4] == 'last_watered'
-        assert result_df.columns[5] == 'recording_taken'
-        assert result_df.columns[6] == 'botanist_name'
-        assert result_df.columns[7] == 'botanist_mobile'
-        assert result_df.columns[8] == 'botanist_email'
-        assert result_df.columns[9] == 'error'
+    result_df = clean_reading_data(fake_df)
+    assert result_df['botanist_email'][0] == "fake_email_1"
+    assert result_df['botanist_email'][1] == "fake_email_2"
 
-    def test_negative_soil_moisture(self, fake_df_negative_soil_temp):
-        """A reading (row) with a negative soil_moisture column value should be discarded."""
 
-        result_df = clean_reading_data(fake_df_negative_soil_temp)
+def test_extracted_botanist_name_column(fake_df):
+    """The botanist column should be flattened to include a botanist_name column."""
 
-        assert result_df["soil_moisture"].get(0) is None
-        assert result_df["soil_moisture"][1] == 30.92
-        assert len(result_df["soil_moisture"]) == 1
+    result_df = clean_reading_data(fake_df)
+    assert result_df['botanist_name'][0] == "fake_name_1"
+    assert result_df['botanist_name'][1] == "fake_name_2"
 
-    def test_negative_temperature(self, fake_df_negative_soil_temp):
-        """A reading (row) with a negative temperature column value should be discarded."""
 
-        result_df = clean_reading_data(fake_df_negative_soil_temp)
+def test_extracted_botanist_mobile_column(fake_df):
+    """The botanist column should be flattened to include a botanist_mobile column."""
 
-        assert result_df["temperature"].get(0) is None
-        assert result_df["temperature"][1] == 9.15
-        assert len(result_df["temperature"]) == 1
+    result_df = clean_reading_data(fake_df)
+    assert result_df['botanist_mobile'][0] == "fake_phone_1"
+    assert result_df['botanist_mobile'][1] == "fake_phone_2"
 
-    def test_high_temperature(self, fake_df_high_soil_temp):
-        """A reading (row) with an excessive temperature column value should be discarded."""
 
-        result_df = clean_reading_data(fake_df_high_soil_temp)
+def test_column_order(fake_df):
+    """
+    The resulting data frame columns should be in a specific order
+    for compatibility with load script.
+    """
 
-        assert result_df["temperature"].get(0) is None
-        assert result_df["temperature"][1] == 9.15
-        assert len(result_df["temperature"]) == 1
+    result_df = clean_reading_data(fake_df)
+    assert result_df.columns[0] == 'plant_id'
+    assert result_df.columns[1] == 'plant_name'
+    assert result_df.columns[2] == 'soil_moisture'
+    assert result_df.columns[3] == 'temperature'
+    assert result_df.columns[4] == 'last_watered'
+    assert result_df.columns[5] == 'recording_taken'
+    assert result_df.columns[6] == 'botanist_name'
+    assert result_df.columns[7] == 'botanist_mobile'
+    assert result_df.columns[8] == 'botanist_email'
+    assert result_df.columns[9] == 'error'
 
-    def test_high_soil_moisture(self, fake_df_high_soil_temp):
-        """A reading (row) with an excessive soil_moisture column value should be discarded."""
 
-        result_df = clean_reading_data(fake_df_high_soil_temp)
+def test_negative_soil_moisture(fake_df_negative_soil_temp):
+    """A reading (row) with a negative soil_moisture column value should be discarded."""
 
-        assert result_df["soil_moisture"].get(0) is None
-        assert result_df["soil_moisture"][1] == 30.92
-        assert len(result_df["soil_moisture"]) == 1
+    result_df = clean_reading_data(fake_df_negative_soil_temp)
 
-    def test_zero_temperature(self, fake_df_zero_soil_temp):
-        """A reading (row) with an zero temperature column value should be kept."""
+    assert result_df["soil_moisture"].get(0) is None
+    assert result_df["soil_moisture"][1] == 30.92
+    assert len(result_df["soil_moisture"]) == 1
 
-        result_df = clean_reading_data(fake_df_zero_soil_temp)
 
-        assert result_df["temperature"][0] == 0
-        assert result_df["temperature"][1] == 9.15
-        assert len(result_df["temperature"]) == 2
+def test_negative_temperature(fake_df_negative_soil_temp):
+    """A reading (row) with a negative temperature column value should be discarded."""
 
-    def test_zero_soil_moisture(self, fake_df_zero_soil_temp):
-        """A reading (row) with an zero soil_moisture column value should be kept."""
+    result_df = clean_reading_data(fake_df_negative_soil_temp)
 
-        result_df = clean_reading_data(fake_df_zero_soil_temp)
+    assert result_df["temperature"].get(0) is None
+    assert result_df["temperature"][1] == 9.15
+    assert len(result_df["temperature"]) == 1
 
-        assert result_df["soil_moisture"][0] == 0
-        assert result_df["soil_moisture"][1] == 30.92
-        assert len(result_df["soil_moisture"]) == 2
+
+def test_high_temperature(fake_df_high_soil_temp):
+    """A reading (row) with an excessive temperature column value should be discarded."""
+
+    result_df = clean_reading_data(fake_df_high_soil_temp)
+
+    assert result_df["temperature"].get(0) is None
+    assert result_df["temperature"][1] == 9.15
+    assert len(result_df["temperature"]) == 1
+
+
+def test_high_soil_moisture(fake_df_high_soil_temp):
+    """A reading (row) with an excessive soil_moisture column value should be discarded."""
+
+    result_df = clean_reading_data(fake_df_high_soil_temp)
+
+    assert result_df["soil_moisture"].get(0) is None
+    assert result_df["soil_moisture"][1] == 30.92
+    assert len(result_df["soil_moisture"]) == 1
+
+
+def test_zero_temperature(fake_df_zero_soil_temp):
+    """A reading (row) with an zero temperature column value should be kept."""
+
+    result_df = clean_reading_data(fake_df_zero_soil_temp)
+
+    assert result_df["temperature"][0] == 0
+    assert result_df["temperature"][1] == 9.15
+    assert len(result_df["temperature"]) == 2
+
+
+def test_zero_soil_moisture(fake_df_zero_soil_temp):
+    """A reading (row) with an zero soil_moisture column value should be kept."""
+
+    result_df = clean_reading_data(fake_df_zero_soil_temp)
+
+    assert result_df["soil_moisture"][0] == 0
+    assert result_df["soil_moisture"][1] == 30.92
+    assert len(result_df["soil_moisture"]) == 2
+
+
+def test_error_reading(fake_df_error):
+    """A reading (row) with nan values due to an error reading should remain as nan."""
+
+    result_df = clean_reading_data(fake_df_error)
+    test_value = pd.isnull(result_df.at[0, 'soil_moisture'])
+    print(test_value)
+
+    assert test_value
