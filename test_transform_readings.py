0b49f96c4ce23ec907538eea6427c9670d79286b
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2023-12-21 14:27:26 +0000

    Testing last_watered column

--- test_transform_readings.py
+++ test_transform_readings.py
@@ -7,6 +7,7 @@ import unittest
 from unittest.mock import patch, MagicMock
 
 import pandas as pd
+from pandas.api.types import is_datetime64_any_dtype as is_datetime
 import pytest
 
 from transform_readings import clean_reading_data
@@ -15,53 +16,44 @@ from transform_readings import clean_reading_data
 @pytest.fixture
 def fake_df():
 
-    fake_plant = {
+    fake_plants = [{
         "botanist": {
             "email": "fake_email_1",
             "name": "fake_name_1",
             "phone": "fake_phone_1"
         },
         "last_watered": "Wed, 20 Dec 2023 14:10:54 GMT",
-        "name": "fake_flower_1",
-        "origin_location": [
-            "7.65649",
-            "4.92235",
-            "fake_location_1",
-            "fake_country_code_1",
-            "fake_region_1"
-        ],
+        "plant_name": "fake_flower_1",
         "plant_id": 1,
+        "error": None,
         "recording_taken": "2023-12-21 10:20:34",
         "soil_moisture": 33.95801666242039,
         "temperature": 9.111493582669517
     },
-    {
+        {
         "botanist": {
             "email": "fake_email_2",
             "name": "fake_name_2",
             "phone": "fake_phone_2"
         },
         "last_watered": "Wed, 20 Dec 2023 14:10:54 GMT",
-        "name": "fake_flower_2",
-        "origin_location": [
-            "7.65649",
-            "4.92235",
-            "fake_location_2",
-            "fake_country_code_2",
-            "fake_region_2"
-        ],
+        "plant_name": "fake_flower_2",
         "plant_id": 2,
+        "error": None,
         "recording_taken": "2023-12-21 10:20:34",
         "soil_moisture": 33.95801666242039,
         "temperature": 9.111493582669517
-    }
+    }]
 
-    return pd.DataFrame([fake_plant])
+    return pd.DataFrame(fake_plants)
 
 
 class TestCleanReadingData():
     """Tests involving clean_reading_data()."""
 
-    def test_(self, fake_df):
+    def test_last_watered_datetime(self, fake_df):
+        """The values in the last_watered column should be converted to a datetime64[ns] type."""
 
-        assert "plant_id" in fake_df.columns
+        result_df = clean_reading_data(fake_df)
+        print(result_df['last_watered'].dtype)
+        assert result_df['last_watered'].dtype == 'datetime64[ns]'
